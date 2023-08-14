7fdd3398b63af4656f179cac67003f19df9d3cfe
ArefinShaon <arefinshaon99@gmail.com>
2023-08-15 04:08:39 +0600

    build file created

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import img from "./assets/c96108ab2f0643c2353339ab6d0dbef5.png";
 const App = () => {
   return (
     <div>
-      <h1 className="banner-title text-2xl md:text-5xl font-bold text-center hover:uppercase  mt-12 "> <span className="text-green-600">Task</span> <span className="text-gray-500">Manager</span> </h1>
+      <h1 className="banner-title text-2xl md:text-5xl font-bold text-center   mt-12 "> <span className="text-green-600">Task</span> <span className="text-gray-500">Manager</span> </h1>
        <div className="m-20  md:m-28 ps-6 md:ps-14">
       <div class="line ">
         <div className="flex -px-2 -ms-12 gap-9">
